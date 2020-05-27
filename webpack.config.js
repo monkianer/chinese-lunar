18235a4086d085c2e9c9d4db2086306d87006969
monkianer <partners.ian@gmail.com>
2020-05-27 22:56:27 +0800

    add webpack output for window.chineseLunar call

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,49 @@
 const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
-  target: 'node',
-  entry: {
-    index: './index.js',
-  },
-  output: {
-    path: path.join(__dirname),
-    filename: 'main.js',
-    libraryTarget: 'commonjs2',
-  },
-  module: { //設定你的檔案選項
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
-      },
+module.exports = [
+  {
+    target: 'node',
+    entry: {
+      index: './index-var.js',
+    },
+    output: {
+      path: path.join(__dirname),
+      filename: 'chinese-lunar.js',
+      libraryTarget: 'var',
+    },
+    module: { //設定你的檔案選項
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+        },
+      ],
+    },
+    plugins: [
+      new webpack.DefinePlugin({ "process.env.DEV": JSON.stringify("production") })
     ],
-  },
-  mode: 'production'
-};
+    mode: 'production'
+  }, {
+    target: 'node',
+    entry: {
+      index: './index-commonjs.js',
+    },
+    output: {
+      path: path.join(__dirname),
+      filename: 'main.js',
+      libraryTarget: 'commonjs2',
+    },
+    module: { //設定你的檔案選項
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+        },
+      ],
+    },
+    mode: 'production'
+  }
+];
